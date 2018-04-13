bba71d9786ad28c0cf9fe3aad2ebfae2e7ef6279
aleph1507 <xrristo@gmail.com>
2018-04-13 17:45:45 +0200

    forms501

---
+++ src/app/form01/form01.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'form01',
+  templateUrl: './form01.component.html',
+  styleUrls: ['./form01.component.css']
+})
+export class Form01Component implements OnInit {
+
+  model = {
+    firstname: "",
+    lastname: ""
+  }
+
+  onSubmit(){
+    
+  }
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
